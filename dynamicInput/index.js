f62046431909f14dbbe5e53bdb037eef6a53f85c
Isa Memmedli <i.a.memmedli@gmail.com>
2024-04-11 21:57:48 +0400

    init

---
+++ dynamicInput/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const DynamicInput = () => {
+  return (
+    <View style={{ width: "100%", height: 56, backgroundColor: "red" }}>
+      <Text>DynamicInput</Text>
+    </View>
+  );
+};
+
+export default DynamicInput;
